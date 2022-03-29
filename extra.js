1366d11069a1a8310a4833806f1c045fc6885675
Alex <forcablaugrana@gmail.com>
2022-03-30 00:31:31 +0300

    cleaned folders

--- extra.js
+++ extra.js
@@ -1,14 +1,10 @@
-let num = 266219;
+'use strict';
 
-num = num.toString(); //  превращаем число в строку
-
-const arr = [...num].map(Number);  // превращаем строку в массив из цифр и присваиваем переменной arr
-
-const arrMulti = arr.reduce((acc, number) => acc * number); // перемножаем все элементы массива методом reduce
-
-const finalResult = arrMulti ** 3; //  возводим результат в 3ю степень
-
-console.log(finalResult); // итоговый результат
-
-console.log(finalResult.toString().substring(0, 2)); // преобразую в строку и отделяю первые 2 символа
-
+const foolProof = function(arg) {
+    if (typeof arg !== 'string') {
+        alert('Нужно ввести строку!');
+    }
+    arg.trim();
+    arg = arg.substr(0,30) + "...";
+    return arg;
+};
