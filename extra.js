961f708f383e377aaf40f3dbda5da631f15eafb2
Alex <forcablaugrana@gmail.com>
2022-04-01 13:08:54 +0300

    lesson07 complete

--- extra.js
+++ extra.js
@@ -1,2 +1,12 @@
 'use strict';
 
+const week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+let date = new Date();
+let today = date.getDay()-1;
+
+
+week.splice(5, 2, week[5].italics(), week[6].italics());
+week.splice(date.getDay()-1, 1, week[date.getDay()-1].bold());
+week.forEach((element, i) => {
+    console.log(element);
+});
