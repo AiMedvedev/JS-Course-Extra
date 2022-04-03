3a6c01d7530afea21cbe308c45d3e50dcca034d5
Alex <forcablaugrana@gmail.com>
2022-04-03 12:31:18 +0300

    tasks complete

--- extra.js
+++ extra.js
@@ -2,11 +2,16 @@
 
 const week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
 let date = new Date();
-let today = date.getDay()-1;
+let currentDay = document.querySelector('.today');
+let day = document.querySelectorAll('.day');
+let today = date.getDay() - 1;
 
 
 week.splice(5, 2, week[5].italics(), week[6].italics());
-week.splice(today, 1, week[today].bold());
+
 week.forEach((element, i) => {
-    console.log(element);
+        day[i].innerHTML = element;
+        if(currentDay) {
+            currentDay.innerHTML = '<b>' + element + '</b>';
+        }
 });
