dad14e3e4785696c7d50279cf5b99d176b2e72f1
Alex <forcablaugrana@gmail.com>
2022-04-04 10:54:08 +0200

    today error fixed

--- extra.js
+++ extra.js
@@ -2,7 +2,6 @@
 
 const week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
 let date = new Date();
-let currentDay = document.querySelector('.today');
 let day = document.querySelectorAll('.day');
 let today = date.getDay() - 1;
 
@@ -11,7 +10,8 @@ week.splice(5, 2, week[5].italics(), week[6].italics());
 
 week.forEach((element, i) => {
         day[i].innerHTML = element;
-        if(currentDay) {
-            currentDay.innerHTML = '<b>' + element + '</b>';
+        if(date) {
+            day[today].innerHTML = '<b>' + day[today].innerHTML + '</b>';
         }
 });
+
