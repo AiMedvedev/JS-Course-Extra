d3fec70bede2ffeedba966e6ff4edbc6c9cce09d
Alex <forcablaugrana@gmail.com>
2022-04-04 14:29:15 +0300

    before homework start

--- extra.js
+++ extra.js
@@ -1,80 +1,103 @@
 'use strict';
 
-// Загадывание случайного числа от 1 до 100
-const isNumber = function(num) {                         
-    return !isNaN(parseFloat(num)) && isFinite(num);  // проверка на число
+const appData = {
+    title: '',
+    screens: [],
+    rollback: 25,
+    screenPrice: 0,
+    adaptive: true,
+    services: {},
+    fullPrice: 0,  
+    allServicePrices: 0,
+    servicePercentPrice: 0,
+    asking: function() {
+        do {
+            appData.title = prompt('Как называется ваш проект?');
+        } 
+        while (appData.isNumber(appData.title));
+                   
+        for (let i = 0; i < 2; i++) {
+            let name;
+            let price = 0;
+            
+            do {
+                name = prompt('Какие типы экранов нужно разработать?');
+            } 
+            while (appData.isNumber(name));
+
+            do {
+                price = prompt('Сколько будет стоить данная работа?');
+            } 
+            while (!appData.isNumber(price));
+
+            appData.screens.push({id: i, name: name, price: price});
+        }
+
+        for (let i = 0; i < 2; i++) {
+            let name;
+            let price = 0;
+
+            do {
+                name = prompt('Какой дополнительный тип услуги нужен?');
+            } 
+            while (appData.isNumber(name));
+
+            do {
+                price = prompt('Сколько это будет стоить?');
+            } 
+            while (!appData.isNumber(price));
+
+            appData.services[name] = +price;
+        }
+
+        appData.adaptive = confirm('Нужен ли адаптив на сайте?');
+    },
+    addPrices: function() {
+        for (let screen of appData.screens) {
+            appData.screenPrice += +screen.price;
+        }
+
+        for(let key in appData.services) {
+            appData.allServicePrices += appData.services[key];
+        }
+    },
+    getFullPrice: function() {
+        appData.fullPrice = +appData.screenPrice + appData.allServicePrices;
+    },
+    isNumber: function(num) {                         
+        return !isNaN(parseFloat(num)) && isFinite(num);  // проверка на число
+    },
+    getRollbackMessage: function(price) {
+        if (price >= 30000) {
+            return 'Даём скидку в 10%';
+        } else if (price < 30000 && price >= 15000) {
+            return 'Даём скидку в 5%';
+        } else if (price < 15000 && price >= 0) {
+            return 'Скидка не предусмотрена';
+        } else {
+            return 'Что-то пошло не так';
+        }
+    },
+    getTitle: function() {
+        appData.title = appData.title.trim()[0].toUpperCase() + appData.title.trim().substring(1).toLowerCase();
+    },
+    getServicePercentPrice: function() {
+        appData.servicePercentPrice = Math.ceil(appData.fullPrice - appData.fullPrice * (appData.rollback / 100));
+    },
+    logger: function() {
+        console.log(appData.fullPrice);
+        console.log(appData.servicePercentPrice);
+        console.log(appData.screens);
+    },
+    start: function() {
+        appData.asking();
+        appData.addPrices();
+        appData.getFullPrice();
+        appData.getServicePercentPrice();
+        appData.getTitle();
+        appData.logger();
+    },
 };
 
-function guessGame (numberX) {
-    return function guessing (userAnswer, i) {    
-        if (i < 1) {
-          let again = confirm('Попытки закончились, хотите сыграть еще?');
-          if (again) {
-            gameStart(answer, i = 10);
-          } else {
-            return;
-          }
-        }  
 
-        if(+userAnswer > numberX) {
-            if (userAnswer === null) {
-                alert('Игра окончена.');
-                return;
-            }
-            if (!isNumber(userAnswer)) {
-                alert('Введите число!');
-                
-            }
-            i--;
-            alert(`Загаданное число меньше. осталось попыток: ${i}`);
-            let newTry = prompt('Введите новый вариант');
-            if (newTry === null) {
-                alert('Игра окончена.');
-                return;
-            } else {
-            userAnswer = +newTry;
-            
-            }
-        }
-        
-        if (+userAnswer < numberX) {
-            if (userAnswer === null) {
-                alert('Игра окончена.');
-                return;
-            }
-        
-            if (!isNumber(userAnswer)) {
-                alert('Введите число!');
-            }
-            i--;
-            alert(`Загаданное число больше: осталось попыток: ${i}`); 
-            let newTry = prompt('Введите новый вариант');
-            if (newTry === null) {
-                alert('Игра окончена.');
-                return;
-            } else {
-            userAnswer = +newTry;
-            }
-        } 
-
-        if (+userAnswer === numberX) {
-            let again = confirm(`Поздравляю, Вы угадали!!! Это было число ${numberX}. Хотели бы сыграть еще?`);
-            if (again) {
-                gameStart(answer, i = 10);
-            } else {
-                return;
-            }
-        }
-
-        guessing(userAnswer, i);
-    };
-}
-
-let count = 10;
-const numberToGuess = () => {return Math.round(Math.random() * 100);};
-let answer = prompt('Угадай число от 1 до 100');
-
-
-let gameStart = guessGame(numberToGuess());
-
-gameStart(answer, count);
+appData.start();
